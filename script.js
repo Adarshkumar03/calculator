11dbe2757fd265bb43052e6ac79052fb08c23a3d
Adarsh Kumar <adarshkumar245@gmail.com>
2021-06-15 10:19:06 +0530

    add operator function

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ const sub = (num1, num2)=>{
  const div = (num1, num2)=>{
     return num1/num2;
  }
+
+ const operator = (operator, num1,num2)=>{
+     switch (operator) {
+         case "+":
+             console.log(add(num1,num2));
+             break;
+         case "-":
+             console.log(sub(num1,num2));
+             break;
+         case "*":
+             console.log(mul(num1,num2));
+             break;
+         case "/":
+             console.log(div(num1,num2));
+             break;     
+         default:console.log("wrong input");
+     }
+ }
